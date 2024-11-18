048ff1e6f1c08848095d133917d7c2bd40d07b59
Chuckytin <angeltorcalcatalan@gmail.com>
2024-11-18 18:19:13 +0100

    corrigiendo ejercicio preparedStatementInsert

--- BDRelacionales/src/main/java/com/example/JDBC_PreparedStatementInsert.java
+++ BDRelacionales/src/main/java/com/example/JDBC_PreparedStatementInsert.java
@@ -10,26 +10,55 @@ public class JDBC_PreparedStatementInsert {
 
     private static void preparedStatementInsert() {
 
+        String tableName = "Clientes1";
+        String sqlCreateTable = "CREATE TABLE IF NOT EXISTS Clientes1 (" +
+                "dni CHAR(9) NOT NULL PRIMARY KEY," +
+                "apellidos VARCHAR(32) NOT NULL," +
+                "codigoPostal Integer" +
+                ")";
+
+        //conexión a la base de datos
         try (Connection connection = DriverManager.getConnection(JDBC_Connection.urlConecciont,
                 JDBC_Connection.user, JDBC_Connection.pwd)) {
 
-            Statement statement = connection.createStatement();
+            //DatabaseMetadata verifica si la tabla existe
+            DatabaseMetaData metaData = connection.getMetaData();
+            ResultSet tables = metaData.getTables(null, null, tableName, new String[]{"TABLE"});
 
-            String sqlCreateTable = "CREATE TABLE Clientes1 (" +
-                    "dni CHAR(9) NOT NULL PRIMARY KEY," +
-                    "apellidos VARCHAR(32) NOT NULL," +
-                    "codigoPostal Integer" +
-                    ")";
+            boolean tableExists = tables.next();
 
-            statement.execute(sqlCreateTable);
+            if (!tableExists) {
+                Statement statement = connection.createStatement();
+                statement.execute(sqlCreateTable);
+                System.out.println("Tabla creada: " + tableName);
+            } else {
+                System.out.println("Tabla ya existe: " + tableName);
+            }
 
-            String sqlInsertInto = "INSERT INTO Clientes1 (dni, apellidos, codigoPostal) VALUES (?, ?, ?)";
+            //Verifica si hay datos en la tabla gracias a la consulta COUNT
+            String sqlCheckData = "SELECT COUNT(*) FROM Clientes1";
+            PreparedStatement checkStatement = connection.prepareStatement(sqlCheckData);
+            ResultSet checkResult = checkStatement.executeQuery();
 
-            PreparedStatement preparedStatement = connection.prepareStatement(sqlInsertInto);
+            boolean hasData = checkResult.next() && checkResult.getInt(1) > 0;
 
-            preparedStatement.setInt(1, 179648260);
-            preparedStatement.setString(2, "Torcal Fernandez");
-            preparedStatement.setInt(3, 50005);
+            //si no hay datos se insertan los datos
+            if (!hasData) {
+                String sqlInsertInto = "INSERT INTO Clientes1 (dni, apellidos, codigoPostal) VALUES (?, ?, ?)";
+
+                PreparedStatement preparedStatement = connection.prepareStatement(sqlInsertInto);
+
+                preparedStatement.setInt(1, 179648260);
+                preparedStatement.setString(2, "Torcal Fernandez");
+                preparedStatement.setInt(3, 50005);
+
+                int rowsInserted = preparedStatement.executeUpdate();
+                System.out.println((rowsInserted == 1) ?
+                        rowsInserted + " fila insertada." : rowsInserted + " filas insertadas.");
+
+            } else {
+                System.out.println("La tabla ya contiene datos.");
+            }
 
         } catch (SQLException e) {
             JDBC_Connection.muestraErrorSQL(e);
